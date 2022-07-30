bdea2b264babe8edc8e80fc17992324a34d3b72e
Сергей Огарков <lifesaver43@yandex.ru>
2022-07-30 20:51:05 +0300

    Example works with Scoped Model on Flutter.

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ScopedModel Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const MyHomePage(title: 'ScopedModel Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ScopedModel Widget'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          ScopedModel<MyModelState>(
+            model: MyModelState(),
+            child: _AppRootWidget(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AppRootWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4.0,
+      child: Column(
+        children: <Widget>[
+          Text('(Root Widget)', style: Theme.of(context).textTheme.headline4),
+          const SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _Counter(),
+              _Counter(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Counter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModelDescendant<MyModelState>(
+      builder: (context, child, model) => Card(
+        margin: const EdgeInsets.all(4.0).copyWith(bottom: 32.0),
+        color: Colors.amberAccent,
+        child: Column(
+          children: <Widget>[
+            const Text('(Child Widget)'),
+            Text('${model.counterValue}',
+                style: Theme.of(context).textTheme.headline4),
+            ButtonBar(
+              children: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.remove),
+                  color: Colors.red,
+                  onPressed: () {
+                    model._decrementCounter();
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  color: Colors.green,
+                  onPressed: () {
+                    model._incrementCounter();
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      rebuildOnChange: true,
+    );
+  }
+}
+
+class MyModelState extends Model {
+  int _counter = 0;
+
+  int get counterValue => _counter;
+
+  void _incrementCounter() {
+    _counter++;
+    notifyListeners();
+  }
+
+  void _decrementCounter() {
+    _counter--;
+    notifyListeners();
+  }
+}
